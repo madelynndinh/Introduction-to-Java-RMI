8e13f24d6969a6c3567ec917613879a7bdca3139
madelynndinh <madelynndinh@gmail.com>
2025-08-14 01:43:22 +0930

    Added CalculatorImplementation skeleton with shared stack and synchronized method stubs

---
+++ src/main/java/org/example/CalculatorImplementation.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Stack;
+
+public class CalculatorImplementation extends UnicastRemoteObject implements Calculator {
+    private final Stack<Integer> stack;
+
+    protected CalculatorImplementation() throws RemoteException {
+        super();
+        this.stack = new Stack<>();
+    }
+
+    @Override
+    public synchronized void pushValue(int val) throws RemoteException {
+    }
+
+    @Override
+    public synchronized void pushOperation(String operator) throws RemoteException {
+    }
+
+    @Override
+    public synchronized int pop() throws RemoteException {
+        return 0;
+    }
+
+    @Override
+    public synchronized boolean isEmpty() throws RemoteException {
+        return true;
+    }
+
+    @Override
+    public synchronized int delayPop(int millis) throws RemoteException {
+        return 0;
+    }
+}
